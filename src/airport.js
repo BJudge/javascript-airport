8e24f632793645acd14d951d3e62a6072122e47e
BartJudge <BartJudge@gmail.com>
2016-12-18 13:04:33 +0000

    complete, but airport not defined

---
+++ src/airport.js
@@ -0,0 +1,20 @@
+'use strict';
+var Airport = function(weather) {
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
+  this._hangar = [];
+};
+Airport.prototype.planes = function() {
+  return this._hangar;
+};
+Airport.prototype.clearForLanding = function(plane) {
+  if(this.isStormy();) {
+    throw new Error('Cannot land during storm');
+  }
+  this._hangar.push(plane);
+};
+Airport.prototype.clearForTakeOff = function(plane) {
+  if(this.isStormy();) {
+    throw new Error('Cannot takeoff during storm');
+  }
+  this._hangar = [];
+};
---
+++ src/plane.js
@@ -0,0 +1,9 @@
+'use strict';
+var Plane = function() {};
+Plane.prototype.land = function(airport) {
+  airport.clearForLanding(this);
+  this._location = airport;
+};
+Plane.prototype.takeoff = function(airport) {
+  this._location.clearForTakeOff();
+};
