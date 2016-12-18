8aacfb9bb732a5b1133f1ea44df939b3c51cdade
BartJudge <BartJudge@gmail.com>
2016-12-18 17:34:07 +0000

    completed airport challenge

--- 2/src/plane.js
+++ 2/src/plane.js
@@ -3,4 +3,8 @@
 var Plane = function() {};
 Plane.prototype.land = function(airport) {
   airport.clearForLanding(this);
+  this._location = airport;
+};
+Plane.prototype.takeoff = function(airport) {
+  this._location.clearForTakeOff();
 };
