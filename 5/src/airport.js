9e2bd252fa08982b72b77d6235aa56976e0a5521
BartJudge <BartJudge@gmail.com>
2016-12-27 22:29:58 +0000

    refactored

--- 5/src/airport.js
+++ 5/src/airport.js
@@ -1,27 +1,24 @@
 'use strict';
 
-var Airport = function() {
- this._hangar = []
-};
+var Airport = function(weather){
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
+  this._hangar = [];
+}
 
-Airport.prototype.planes = function() {
+Airport.prototype.planes = function(){
   return this._hangar;
 };
 
 Airport.prototype.clearForLanding = function(plane) {
-  if (this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot land during storm');
   }
   this._hangar.push(plane);
 };
 
 Airport.prototype.clearForTakeOff = function(plane) {
-  if (this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot takeoff during storm');
   }
   this._hangar = [];
-};
-
-Airport.prototype.isStormy = function() {
-  return false;
 };
