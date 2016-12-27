40a4ca8e3cd097b54505f108948a9cc9881615b4
BartJudge <BartJudge@gmail.com>
2016-12-27 22:16:10 +0000

    cannot take off during storm

--- 5/src/airport.js
+++ 5/src/airport.js
@@ -3,12 +3,22 @@
 var Airport = function() {
  this._hangar = []
 };
+
 Airport.prototype.planes = function() {
   return this._hangar;
 };
+
 Airport.prototype.clearForLanding = function(plane) {
   this._hangar.push(plane);
 };
+
 Airport.prototype.clearForTakeOff = function(plane) {
+  if (this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
   this._hangar = [];
+};
+
+Airport.prototype.isStormy = function() {
+  return false;
 };
